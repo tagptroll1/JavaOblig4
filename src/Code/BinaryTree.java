421039ab00606e2dbb9579a7f92a1968ecca78bf
Tagptroll1 <thomas@galehuset.org>
2018-05-15 20:38:41 +0200

    LinkedStack implemented

--- src/Code/BinaryTree.java
+++ src/Code/BinaryTree.java
@@ -1,6 +1,10 @@
 package Code;
 
 import Exception.EmptyTreeException;
+import Interface.StackInterface;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class BinaryTree<T> {
     private BinaryNode<T> root;
@@ -82,4 +86,83 @@ public class BinaryTree<T> {
         this.root = null;
     }
 
+    @SuppressWarnings("unchecked")
+    public Iterable<T> getInorderIterator(){
+        return (Iterable<T>) new InorderIterator();
+    }
+
+    private class InorderIterator implements Iterator<T> {
+        private StackInterface<BinaryNode<T>> nodeStack;
+        private BinaryNode<T> currentNode;
+
+        public InorderIterator(){
+            nodeStack = new LinkedStack<>();
+            currentNode = root;
+        }
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return !nodeStack.isEmpty() || (currentNode!= null);
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public T next() {
+            BinaryNode<T> nextNode = null;
+
+            while(currentNode != null){
+                nodeStack.push(currentNode);
+                currentNode = currentNode.getLeft();
+            }
+
+            if (!nodeStack.isEmpty()){
+                nextNode = nodeStack.pop();
+                assert nextNode != null;
+
+                currentNode = nextNode.getRight();
+            } else {
+                throw new NoSuchElementException();
+            }
+
+            return nextNode.getData();
+        }
+
+        /**
+         * Removes from the underlying collection the last element returned
+         * by this iterator (optional operation).  This method can be called
+         * only once per call to {@link #next}.
+         * <p>
+         * The behavior of an iterator is unspecified if the underlying collection
+         * is modified while the iteration is in progress in any way other than by
+         * calling this method, unless an overriding class has specified a
+         * concurrent modification policy.
+         * <p>
+         * The behavior of an iterator is unspecified if this method is called
+         * after a call to the {@link #forEachRemaining forEachRemaining} method.
+         *
+         * @throws UnsupportedOperationException if the {@code remove}
+         *                                       operation is not supported by this iterator
+         * @throws IllegalStateException         if the {@code next} method has not
+         *                                       yet been called, or the {@code remove} method has already
+         *                                       been called after the last call to the {@code next}
+         *                                       method
+         * @implSpec The default implementation throws an instance of
+         * {@link UnsupportedOperationException} and performs no other action.
+         */
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
